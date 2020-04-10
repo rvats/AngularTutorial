b5bfa40892f7d8e66853aab80d50180d7caec32a
Rahul Vats <vats.rahul@gmail.com>
2020-04-10 06:34:53 +0000

    Updating Code To Display a list of data objects

--- tutorial/twi/app/components/list/list.component.ts
+++ tutorial/twi/app/components/list/list.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'twi-list',
   templateUrl: './list.component.html',
 })
 export class ListComponent implements OnInit {
+  @Input()
+  dataList: object[];
   constructor() {}
 
   ngOnInit(): void {}
--- tutorial/twi/app/twi-component/twi.component.ts
+++ tutorial/twi/app/twi-component/twi.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'twi-root',
-  templateUrl: './twi.component.html'
+  templateUrl: './twi.component.html',
 })
-export class TwiComponent {
-  title = 'tutorial';
+export class TwiComponent implements OnInit {
+  title = 'Rahul Vats Anular Tutorial';
+  dataList: object[];
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.httpClient.get<object[]>('../assets/data.json').subscribe((data) => {
+      this.dataList = data;
+    });
+  }
 }
