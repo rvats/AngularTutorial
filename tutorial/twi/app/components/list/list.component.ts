38badc9c84bf153e2f3e7d4443145a84994cc364
Rahul Vats <vats.rahul@gmail.com>
2020-04-10 05:04:05 +0000

    Updating and adding new Componenets

---
+++ tutorial/twi/app/components/list/list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'twi-list',
+  templateUrl: './list.component.html'
+})
+export class ListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- tutorial/twi/app/twi.module.ts
+++ tutorial/twi/app/twi.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { TwiComponent } from './twi.component';
+import { AddComponent } from './components/add/add.component';
+import { ListComponent } from './components/list/list.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    TwiComponent
+    TwiComponent,
+    AddComponent,
+    ListComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
