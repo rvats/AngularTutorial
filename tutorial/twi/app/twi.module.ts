3ddde3b872d8c21a343249381e96f29bc7240c0e
Rahul Vats <vats.rahul@gmail.com>
2020-04-10 05:36:16 +0000

    Adding Data.json

--- tutorial/twi/app/twi.module.ts
+++ tutorial/twi/app/twi.module.ts
@@ -1,22 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { TwiComponent } from './twi.component';
+import { TwiComponent } from './twi-component/twi.component';
 import { AddComponent } from './components/add/add.component';
 import { ListComponent } from './components/list/list.component';
 import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
-  declarations: [
-    TwiComponent,
-    AddComponent,
-    ListComponent,
-    SearchComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [TwiComponent, AddComponent, ListComponent, SearchComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [TwiComponent]
+  bootstrap: [TwiComponent],
 })
-export class TwiModule { }
+export class TwiModule {}
