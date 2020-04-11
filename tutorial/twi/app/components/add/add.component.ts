895c8916542867ed90538c2daf685aef87e5b654
Rahul Vats <vats.rahul@gmail.com>
2020-04-11 20:42:07 +0000

    Adding Article Submission Form

--- tutorial/twi/app/components/add/add.component.ts
+++ tutorial/twi/app/components/add/add.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'twi-add',
   templateUrl: './add.component.html',
 })
 export class AddComponent implements OnInit {
-  constructor() {}
+  showForm: boolean;
+  faPlusCircle = faPlusCircle;
+
+  constructor() {
+    this.showForm = true;
+  }
 
   ngOnInit(): void {}
+
+  toggleArticleFormDisplay() {
+    this.showForm = !this.showForm;
+  }
 }
