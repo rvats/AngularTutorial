bb36fd927bf8d28e02c96b6d198e68ef472c51ac
Rahul Vats <vats.rahul@gmail.com>
2020-04-12 06:56:21 +0000

    Modified Data Sorting and Add More Test Data

--- tutorial/twi/app/twi-component/twi.component.ts
+++ tutorial/twi/app/twi-component/twi.component.ts
@@ -1,40 +1,68 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import {without} from 'lodash';
+import { without } from 'lodash';
 
 @Component({
   selector: 'twi-root',
   templateUrl: './twi.component.html',
 })
 export class TwiComponent implements OnInit {
-  title = 'Rahul Vats Anular Tutorial';
   dataList: object[];
+  modifiedDataList: object[];
+  orderBy: string;
+  orderType: string;
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {
+    this.orderBy = 'articleName';
+    this.orderType = 'asc';
+  }
 
   ngOnInit(): void {
     this.httpClient.get<object[]>('../assets/data.json').subscribe((data) => {
       this.dataList = data;
+      this.modifiedDataList = data;
+      this.sortArticles();
     });
   }
 
-  addArticle(article: object){
+  addArticle(article: object) {
     this.dataList.unshift(article);
+    this.modifiedDataList.unshift(article);
   }
 
-  deleteArticle(article: object){
+  deleteArticle(article: object) {
     this.dataList = without(this.dataList, article);
+    this.modifiedDataList = without(this.dataList, article);
   }
 
-  queryArticles(query: string){
-    this.dataList = this.dataList.filter(
-      eachItem => {
-        return (
-          eachItem['articleName'].toLowerCase().includes(query.toLowerCase()) ||
-          eachItem['articleUrl'].toLowerCase().includes(query.toLowerCase()) ||
-          eachItem['articleAuthor'].toLowerCase().includes(query.toLowerCase())
-        )
+  queryArticles(query: string) {
+    this.modifiedDataList = this.dataList.filter((eachItem) => {
+      return (
+        eachItem['articleName'].toLowerCase().includes(query.toLowerCase()) ||
+        eachItem['articleUrl'].toLowerCase().includes(query.toLowerCase()) ||
+        eachItem['articleAuthor'].toLowerCase().includes(query.toLowerCase())
+      );
+    });
+  }
+
+  sortArticles() {
+    let order: number;
+    if (this.orderType === 'asc') {
+      order = 1;
+    } else {
+      order = -1;
+    }
+
+    this.modifiedDataList.sort((a, b) => {
+      if (a[this.orderBy].toLowerCase() < b[this.orderBy].toLowerCase()) {
+        return -1 * order;
       }
-    );
+      if (a[this.orderBy].toLowerCase() === b[this.orderBy].toLowerCase()) {
+        return 0;
+      }
+      if (a[this.orderBy].toLowerCase() > b[this.orderBy].toLowerCase()) {
+        return 1 * order;
+      }
+    });
   }
 }
