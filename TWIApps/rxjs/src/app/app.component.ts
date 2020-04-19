95e62a0f54e789c4ee77e3acf407163f001597e5
Rahul Vats <vats.rahul@gmail.com>
2020-04-19 13:53:29 -0500

    interval take and map demo

--- TWIApps/rxjs/src/app/app.component.ts
+++ TWIApps/rxjs/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { interval } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { take, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,20 +10,23 @@ import { take } from 'rxjs/operators';
 
 export class AppComponent implements OnInit, OnDestroy {
   title = 'rxjs';
-  subject$;
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    const number$ = interval(1000).pipe(take(10));
+    const number$ = interval(1000);
 
-    number$.subscribe(x => console.log('Next: ', x));
+    number$
+      .pipe(
+        take(11),
+        map(x => x*x)
+      )
+      .subscribe(x => console.log('Next: ', x));
   }
 
   ngOnDestroy(): void {
     //Called once, before the instance is destroyed.
     //Add 'implements OnDestroy' to the class.
-    this.subject$.unsubscribe();
-    // Above line will clean up any memory leaks issue due to subjects
+    
   }
 }
