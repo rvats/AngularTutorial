c9cd26146e169c7457e77428aa70e3af95ff3a6a
Rahul Vats <vats.rahul@gmail.com>
2020-04-19 13:47:48 -0500

    interval and take operators

--- TWIApps/rxjs/src/app/app.component.ts
+++ TWIApps/rxjs/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
-
+import { interval } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,14 +15,9 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.subject$ = new ReplaySubject(200);
-    this.subject$.subscribe(x => console.log('First Subscriber ', x));
-    this.subject$.next(1);
-    this.subject$.next(2);
-    this.subject$.next(3);
-    // To complete and kill any further subscription use unsubscribe and not complete.
-    this.subject$.subscribe(x=> console.log('Second Subscriber ', x));
-    this.subject$.next(4);
+    const number$ = interval(1000).pipe(take(10));
+
+    number$.subscribe(x => console.log('Next: ', x));
   }
 
   ngOnDestroy(): void {
