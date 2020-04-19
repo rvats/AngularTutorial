cc88ddd48be4a5fa8b1e563744ee8d05cf0e87d6
Rahul Vats <vats.rahul@gmail.com>
2020-04-19 00:13:46 -0500

    Adding RXJS Observable

--- TWIApps/rxjs/src/app/app.component.ts
+++ TWIApps/rxjs/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit, OnDestroy {
   title = 'rxjs';
+  observable$;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.observable$ = Observable.create((observer)=>{
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete();
+    });
+    this.observable$.subscribe(
+      value => console.log(value),
+      err => console.error('Something went wrong'),
+      () => console.log('this is the end.')
+    );
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.observable$.unsubscribe();
+    // Above line will clean up any memory leaks issue due to observables
+  }
 }
