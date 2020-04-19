93a560e8b21443099d46ca48846e88d66d286fbc
Rahul Vats <vats.rahul@gmail.com>
2020-04-18 23:31:21 -0500

    Updating angular.json as needed.

---
+++ CleanUpWorkSpace.bat
@@ -0,0 +1,5 @@
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S debug') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S plugin') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S node_modules') DO RMDIR /S /Q "%%G"
