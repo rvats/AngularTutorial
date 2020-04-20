15407e334318812af33f3852196df0c97e653537
Rahul Vats <vats.rahul@gmail.com>
2020-04-20 10:55:55 -0500

    Getting mergeMap to work

--- TWIApps/rxjs/src/app/app.component.ts
+++ TWIApps/rxjs/src/app/app.component.ts
@@ -1,32 +1,27 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval } from 'rxjs';
-import { map, take, filter } from 'rxjs/operators';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { interval, of } from "rxjs";
+import { mergeMap, map, take } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-
 export class AppComponent implements OnInit, OnDestroy {
-  title = 'rxjs';
+  title = "rxjs";
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    const numbers$ = interval(1000);
-    numbers$
-      .pipe(
-        take(11)
-      ).pipe(
-        map(x => x * x),
-        filter(x => x > 15)
-      ).subscribe(x => console.log(x));
+
+    const letters$ = of("a", "b", "c", "d", "e");
+    const numbers$ = letters$
+      .pipe(mergeMap((x) => interval(1000).pipe(take(6),map((i) => x + i))))
+      .subscribe((x) => console.log(x));
   }
 
   ngOnDestroy(): void {
     //Called once, before the instance is destroyed.
     //Add 'implements OnDestroy' to the class.
-    
   }
 }
