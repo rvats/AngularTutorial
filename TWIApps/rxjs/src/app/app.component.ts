34db5a5500ca733c1e0055ee03b8bc1061cce1a6
Rahul Vats <vats.rahul@gmail.com>
2020-04-19 21:40:17 -0500

    Need to get this moved to right event may be ngChanges

--- TWIApps/rxjs/src/app/app.component.ts
+++ TWIApps/rxjs/src/app/app.component.ts
@@ -1,28 +1,47 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { fromEvent } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-
 export class AppComponent implements OnInit, OnDestroy {
-  title = 'rxjs';
+  title = "rxjs";
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    const clicks = fromEvent(document, 'click');
-    clicks.subscribe(x=>console.log(x.clientX));
-    const positions = clicks.pipe(map(ev => ev.clientY));
-    positions.subscribe(x => console.log(x));
+    const clicks = fromEvent(document, "click");
+    const positions = clicks.pipe(
+      map(
+        (ev) =>
+          function (ev) {
+            return {
+              clientX: ev.clientX,
+              clientY: ev.clientY,
+            };
+          }
+      )
+    );
+    positions.subscribe(
+      (x) =>
+        function writeClickCoordinatesToConsole(x) {
+          if (x) {
+            console.log(
+              "You clicked at Coordinate X: ",
+              x.clientX,
+              ", Coordinate Y: ",
+              x.clientY
+            );
+          }
+        }
+    );
   }
 
   ngOnDestroy(): void {
     //Called once, before the instance is destroyed.
     //Add 'implements OnDestroy' to the class.
-    
   }
 }
